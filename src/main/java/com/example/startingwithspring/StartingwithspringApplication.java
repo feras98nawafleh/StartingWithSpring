cd0cbfaca849068729ded15c0d938c5be9f1e882
feras98nawafleh <feras.a.nawafleh@gmail.com>
2022-03-14 20:21:14 +0300

    initial commit

---
+++ src/main/java/com/example/startingwithspring/StartingwithspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.startingwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartingwithspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartingwithspringApplication.class, args);
+	}
+
+}
